24181809d37b0f1ae2066e8c2d1cf93cfb3d49c2
wikshitha <uminduwikshitha@gmail.com>
2026-07-10 00:18:51 +0530

    Project Setup and Install Required Packages

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ServicesModule } from './services/services.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UsersModule,
+    ServicesModule,
+    BookingsModule,
+    PrismaModule,
+  ],
+})
+export class AppModule {}
